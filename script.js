c886ca32de0fc81f0f7eb7ddb3c1056abe5f8fbb
Bereket Besha <104008968+berikila@users.noreply.github.com>
2022-11-23 21:51:04 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+		gsap.to("#text, .outline-text", {
+			scrollTrigger:{
+				trigger: ".text, .outline-text", 
+				start: "center center", 
+				end: "bottom top", 
+				scrub: 1
+			},
+			x: 100
+		})
+
+		gsap.to("#text2", {
+			scrollTrigger:{
+				trigger: "#text2",
+				start: "bottom bottom", 
+				end: "bottom top", 
+				scrub: 1,
+				markers: true
+			},
+			x: -150,
+
+		})
